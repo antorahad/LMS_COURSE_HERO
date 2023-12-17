810718cdb00fd4c76cc9b46af23690ff9b18e27f
antorahad <antorahad6@gmail.com>
2023-12-18 00:22:26 +0600

    update

--- src/Error/Error.jsx
+++ src/Error/Error.jsx
@@ -1,11 +1,19 @@
+import { Link } from 'react-router-dom';
 import error404 from '../assets/404.png'
+import { Helmet } from 'react-helmet-async';
 
 const Error = () => {
     return (
-        <div className="flex items-center justify-center min-h-screen">
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <Helmet>
+                <title>Error Page</title>
+             </Helmet>
             <div>
                 <img src={error404} alt="error image" />
             </div>
+            <Link to={'/'}>
+                <a className='text-green-500 underline'>Retun To Home</a>
+            </Link>
         </div>
     );
 };
